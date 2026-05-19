5ee94d08bdcd96073aaa03a9fda3fcdeb8eba0a1
vaibhav212303 <vaibhavp@testrig.co.in>
2026-05-19 17:53:06 +0530

    added correct folder path

--- playwright_server/deploy.sh
+++ playwright_server/deploy.sh
@@ -4,7 +4,7 @@
 CONTAINER_NAME="playwright-api"
 IMAGE_NAME="playwright-server"
 PORT="7860"
-REPO_PATH="/Knowledge_Gen/playwright_server" # Update this to your folder path
+REPO_PATH="/Knowledge_Gen/" # Update this to your folder path
 
 # --- PRE-FLIGHT CHECKS ---
 echo "🚀 Starting Deployment..."
